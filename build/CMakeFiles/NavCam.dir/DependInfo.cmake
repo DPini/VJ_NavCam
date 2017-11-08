e4296a1b07edba7487a0beb9c7dfd495593517f6
David Pinilla Caparrós <dpinitux@gmail.com>
2017-11-08 16:09:30 +0100

    Initial Commit

---
+++ build/CMakeFiles/NavCam.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dpini/Documentos/Uni/q7/VJ/NavCam/Game.cpp" "/home/dpini/Documentos/Uni/q7/VJ/NavCam/build/CMakeFiles/NavCam.dir/Game.cpp.o"
+  "/home/dpini/Documentos/Uni/q7/VJ/NavCam/Level.cpp" "/home/dpini/Documentos/Uni/q7/VJ/NavCam/build/CMakeFiles/NavCam.dir/Level.cpp.o"
+  "/home/dpini/Documentos/Uni/q7/VJ/NavCam/Scene.cpp" "/home/dpini/Documentos/Uni/q7/VJ/NavCam/build/CMakeFiles/NavCam.dir/Scene.cpp.o"
+  "/home/dpini/Documentos/Uni/q7/VJ/NavCam/Shader.cpp" "/home/dpini/Documentos/Uni/q7/VJ/NavCam/build/CMakeFiles/NavCam.dir/Shader.cpp.o"
+  "/home/dpini/Documentos/Uni/q7/VJ/NavCam/ShaderProgram.cpp" "/home/dpini/Documentos/Uni/q7/VJ/NavCam/build/CMakeFiles/NavCam.dir/ShaderProgram.cpp.o"
+  "/home/dpini/Documentos/Uni/q7/VJ/NavCam/Texture.cpp" "/home/dpini/Documentos/Uni/q7/VJ/NavCam/build/CMakeFiles/NavCam.dir/Texture.cpp.o"
+  "/home/dpini/Documentos/Uni/q7/VJ/NavCam/main.cpp" "/home/dpini/Documentos/Uni/q7/VJ/NavCam/build/CMakeFiles/NavCam.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/SOIL"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/NavCam.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NavCam.dir/Level.cpp.o"
+  "CMakeFiles/NavCam.dir/Texture.cpp.o"
+  "CMakeFiles/NavCam.dir/Scene.cpp.o"
+  "CMakeFiles/NavCam.dir/Shader.cpp.o"
+  "CMakeFiles/NavCam.dir/ShaderProgram.cpp.o"
+  "CMakeFiles/NavCam.dir/Game.cpp.o"
+  "CMakeFiles/NavCam.dir/main.cpp.o"
+  "NavCam.pdb"
+  "NavCam"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NavCam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
